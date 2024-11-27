ae039b082ea9449fceee584813789a0b0aa01963
Danilo Sucur <danilo_sucur@epam.com>
2024-11-27 14:29:05 +0100

    Initial state

--- src/main/java/com/example/demo/chat/dto/ChatResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.chat.dto;
-
-import lombok.Builder;
-
-@Builder
-public record ChatResponse(
-        String aiResponse
-) {
-}
--- src/main/java/com/example/demo/chat/service/ChatService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.chat.service;
-
-import com.example.demo.chat.dto.ChatResponse;
-
-public interface ChatService {
-    ChatResponse getChatResponse(String prompt);
-}
